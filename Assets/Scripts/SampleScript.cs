0763fae3ab42026ed43ffa5ed7a5804028a6cc7b
OmarElSayed97 <omarihabelsayed@gmail.com>
2021-03-14 21:24:31 +0200

    Adding Sample Script

---
+++ Assets/Scripts/SampleScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour
+{
+    #region Dependencies
+    #endregion
+
+
+    #region Main Methods
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+    #endregion
+
+    #region Methods
+    #endregion
+
+    #region Triggers
+    #endregion
+
+    #region Coroutines
+    #endregion
+
+}
